07bfa3ea8214282d997345d51f85841ec405e406
manuelgirbal <manuelgirbal@hotmail.com>
2023-05-26 21:48:21 -0300

    with the bitnodes temporal solution

---
+++ BTCapp/bitnodes.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(httr)
+library(jsonlite)
+
+## Getting nodes data from [Bitnodes](https://bitnodes.io/)
+
+# Define the API endpoint URL
+url2 <- "https://bitnodes.io/api/v1/snapshots/latest/"
+
+# Send the HTTP request to the API endpoint
+response2 <- GET(url2)
+
+# Convert the response to a JSON object
+json_data2 <- fromJSON(content(response2, "text"))
+
+# Extract the nodes data from the JSON object
+nodes <- json_data2$nodes
+
+node_ids <- names(nodes)
+
+nodes_df <- as_tibble(data.frame(node_ids, do.call(rbind, nodes), stringsAsFactors = FALSE))
+
+colnames(nodes_df) <- c("node_id", "protocol", "user_agent", "connected_since", "services", "height", "hostname", "city", "country_code", "latitude", "longitude", "timezone", "asn", "organization_name")
+
+nodes_df$latitude <- as.numeric(nodes_df$latitude)
+nodes_df$longitude <- as.numeric(nodes_df$longitude)
+
+write.csv(nodes_df, file = "BTCapp/nodes.csv")
--- BTCapp/global.R
+++ BTCapp/global.R
@@ -38,26 +38,29 @@ yearly <- btcprice %>%
 
 ## Getting nodes data from [Bitnodes](https://bitnodes.io/)
 
-# Define the API endpoint URL
-url2 <- "https://bitnodes.io/api/v1/snapshots/latest/"
+# # Define the API endpoint URL
+# url2 <- "https://bitnodes.io/api/v1/snapshots/latest/"
+#
+# # Send the HTTP request to the API endpoint
+# response2 <- GET(url2)
+#
+# # Convert the response to a JSON object
+# json_data2 <- fromJSON(content(response2, "text"))
+#
+# # Extract the nodes data from the JSON object
+# nodes <- json_data2$nodes
 
-# Send the HTTP request to the API endpoint
-response2 <- GET(url2)
+# node_ids <- names(nodes)
+#
+# nodes_df <- as_tibble(data.frame(node_ids, do.call(rbind, nodes), stringsAsFactors = FALSE))
+#
+# colnames(nodes_df) <- c("node_id", "protocol", "user_agent", "connected_since", "services", "height", "hostname", "city", "country_code", "latitude", "longitude", "timezone", "asn", "organization_name")
+#
+# nodes_df$latitude <- as.numeric(nodes_df$latitude)
+# nodes_df$longitude <- as.numeric(nodes_df$longitude)
 
-# Convert the response to a JSON object
-json_data2 <- fromJSON(content(response2, "text"))
-
-# Extract the nodes data from the JSON object
-nodes <- json_data2$nodes
-
-node_ids <- names(nodes)
-
-nodes_df <- as_tibble(data.frame(node_ids, do.call(rbind, nodes), stringsAsFactors = FALSE))
-
-colnames(nodes_df) <- c("node_id", "protocol", "user_agent", "connected_since", "services", "height", "hostname", "city", "country_code", "latitude", "longitude", "timezone", "asn", "organization_name")
-
-nodes_df$latitude <- as.numeric(nodes_df$latitude)
-nodes_df$longitude <- as.numeric(nodes_df$longitude)
+library(readr)
+nodes_df <- read.csv("nodes.csv")
 
 # Creating map
 world <- map_data("world")
